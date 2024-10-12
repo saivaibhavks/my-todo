3a6eadf6709d1af194f0b9f62550dfbd28e5bb0c
Saivaibhav K S (OSV) <saivaibhav.s.osv@fedex.com>
2024-10-12 11:58:54 +0530

    added logc

--- src/Content.js
+++ src/Content.js
@@ -1,7 +1,16 @@
 const Content = ({ todos }) => {
+  console.log("todos", todos);
   if (todos.length === 0) {
     return <h1>No Tasks Added currently</h1>;
   }
+
+  return (
+    <div>
+      {todos.map((item, index) => (
+        <h1 key={index}>{item}</h1>
+      ))}
+    </div>
+  );
 };
 
 export default Content;
